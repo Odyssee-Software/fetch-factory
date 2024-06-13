e838cb1c68c35854bcc493c6d9fd8523de847364
MortallicaXxX <MortallicaXxX@hotmail.com>
2024-06-13 14:27:57 +0000

    Clean dist folder

--- dist/request.d.ts
+++
@@ -1,6 +0,0 @@
-import { IFetchFactory } from './index';
-export declare const Get: (headers?: IFetchFactory['headers']) => <InputData extends Record<string, any> = any, OutputData = any>(optionalHeaders?: HeadersInit) => (endpoint: string, data?: InputData) => Promise<any>;
-export declare const Post: (headers?: IFetchFactory['headers']) => <InputData extends Record<string, any> = any, OutputData = any>(optionalHeaders?: HeadersInit) => (endpoint: string, data?: InputData) => Promise<any>;
-export declare const Put: (headers?: IFetchFactory['headers']) => <InputData extends Record<string, any> = any, OutputData = any>(optionalHeaders?: HeadersInit) => (endpoint: string, data?: InputData) => Promise<any>;
-export declare const Patch: (headers?: IFetchFactory['headers']) => <InputData extends Record<string, any> = any, OutputData = any>(optionalHeaders?: HeadersInit) => (endpoint: string, data?: InputData) => Promise<any>;
-export declare const Delete: (headers?: IFetchFactory['headers']) => <InputData extends Record<string, any> = any, OutputData = any>(optionalHeaders?: HeadersInit) => (endpoint: string, data?: InputData) => Promise<any>;
