8a6495ca8e0b0e5a768296a25e879abffd226e71
MortallicaXxX <MortallicaXxX@hotmail.com>
2024-06-14 10:40:20 +0000

    Add generated files

--- dist/request.d.ts
+++ dist/request.d.ts
@@ -1,6 +1,25 @@
 import { IFetchFactory } from './index';
-export declare const Get: (headers?: IFetchFactory['headers']) => <InputData extends Record<string, any> = any, OutputData = any>(optionalHeaders?: HeadersInit) => (endpoint: string, data?: InputData) => Promise<any>;
-export declare const Post: (headers?: IFetchFactory['headers']) => <InputData extends Record<string, any> = any, OutputData = any>(optionalHeaders?: HeadersInit) => (endpoint: string, data?: InputData) => Promise<any>;
-export declare const Put: (headers?: IFetchFactory['headers']) => <InputData extends Record<string, any> = any, OutputData = any>(optionalHeaders?: HeadersInit) => (endpoint: string, data?: InputData) => Promise<any>;
-export declare const Patch: (headers?: IFetchFactory['headers']) => <InputData extends Record<string, any> = any, OutputData = any>(optionalHeaders?: HeadersInit) => (endpoint: string, data?: InputData) => Promise<any>;
-export declare const Delete: (headers?: IFetchFactory['headers']) => <InputData extends Record<string, any> = any, OutputData = any>(optionalHeaders?: HeadersInit) => (endpoint: string, data?: InputData) => Promise<any>;
+export interface ICRUDFactory {
+    /** La fonction utilisée pour effectuer la requête HTTP. */
+    caller: IFetchFactory["caller"];
+    /** Les en-têtes de la requête HTTP. */
+    headers?: IFetchFactory["headers"];
+    parser?: IFetchFactory["parser"];
+}
+export declare function RequestFactory<Response = any>(options: ICRUDFactory): {
+    Get: <InputData extends Record<string, any> = any, OutputData = any>(optionalHeaders?: HeadersInit) => (endpoint: string, data?: InputData) => Promise<Response & {
+        output_data?: OutputData;
+    }>;
+    Post: <InputData extends Record<string, any> = any, OutputData = any>(optionalHeaders?: HeadersInit) => (endpoint: string, data?: InputData) => Promise<Response & {
+        output_data?: OutputData;
+    }>;
+    Put: <InputData extends Record<string, any> = any, OutputData = any>(optionalHeaders?: HeadersInit) => (endpoint: string, data?: InputData) => Promise<Response & {
+        output_data?: OutputData;
+    }>;
+    Patch: <InputData extends Record<string, any> = any, OutputData = any>(optionalHeaders?: HeadersInit) => (endpoint: string, data?: InputData) => Promise<Response & {
+        output_data?: OutputData;
+    }>;
+    Delete: <InputData extends Record<string, any> = any, OutputData = any>(optionalHeaders?: HeadersInit) => (endpoint: string, data?: InputData) => Promise<Response & {
+        output_data?: OutputData;
+    }>;
+};
